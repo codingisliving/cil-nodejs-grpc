436a3fc55393102b6eec3a3cac37d988864fcc22
Ericko Yap <ericko.yaputro@btpn.com>
2021-02-28 10:51:13 +0700

    - Refactor client code. - Remove minimist

--- src/grpc/client.js
+++ src/grpc/client.js
@@ -1,8 +1,11 @@
-const PROTO_PATH = __dirname + '/protos/users.proto';
-
-const parseArgs = require('minimist');
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
+
+const PROTO_PATH = __dirname + '/protos/users.proto';
+const server = {
+  host: 'localhost',
+  port: '50051'
+};
 
 const packageDefinition = protoLoader.loadSync(
     PROTO_PATH,
@@ -15,54 +18,15 @@ const packageDefinition = protoLoader.loadSync(
 const usersProto = grpc.loadPackageDefinition(packageDefinition).users;
 
 const main = () => {
-    const argv = parseArgs(process.argv.slice(2), {
-        string: 'target'
-    });
-    let target;
-
-    if (argv.target) {
-        target = argv.target;
-    } else {
-        target = 'localhost:50051';
-    }
+    const { host, port } = server;
+    const target = `${host}:${port}`;
 
     const client = new usersProto.Users(target,
         grpc.credentials.createInsecure());
 
-    let user;
-    if (argv._.length > 0) {
-        user = argv._[0];
-    } else {
-        user = 'world';
-    }
-
-    client.getAllUsers({}, (err, response) => {
-      console.log(response);
-    });
-
     client.getUser({ id: 'ry6qKDccw' }, (err, response) => {
-      console.log(response);
+       console.log(err || response);
     });
-
-    client.createUser({
-        "name": "Jim Morrison",
-        "bankId": "rypNYv5qD",
-        "amount": "9998800"
-    }, (err, response) => {
-        console.log(response);
-    });
-
-    client.updateUser({
-        "id": "HyvWtCyfu",
-        "bankId": "rypNYv5qD",
-        "amount": "9998800"
-    }, (err, response) => {
-        console.log(response);
-    });
-
-    client.deleteUser({ id: 'SyQa5Ryf_'}, (err, response) => {
-        console.log(response);
-    })
 }
 
 main();
