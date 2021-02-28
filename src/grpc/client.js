45149e3714486690003b0dddb019d5ea4843b172
Ericko Yap <ericko.yaputro@btpn.com>
2021-02-28 10:56:11 +0700

    - Refactor client code

--- src/grpc/client.js
+++ src/grpc/client.js
@@ -24,9 +24,33 @@ const main = () => {
     const client = new usersProto.Users(target,
         grpc.credentials.createInsecure());
 
+    client.getAllUsers({}, (err, response) => {
+      console.log(response);
+    });
+
     client.getUser({ id: 'ry6qKDccw' }, (err, response) => {
        console.log(err || response);
     });
+
+    client.createUser({
+        "name": "Jim Morrison",
+        "bankId": "rypNYv5qD",
+        "amount": "9998800"
+    }, (err, response) => {
+        console.log(response);
+    });
+
+    client.updateUser({
+        "id": "HyvWtCyfu",
+        "bankId": "rypNYv5qD",
+        "amount": "9998800"
+    }, (err, response) => {
+        console.log(response);
+    });
+
+    client.deleteUser({ id: 'SyQa5Ryf_'}, (err, response) => {
+        console.log(response);
+    })
 }
 
 main();
